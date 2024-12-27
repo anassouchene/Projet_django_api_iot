d4c02e4e9208987a9af156a5444aebd492ed875b
anassouchene <anassouchene27@gmail.com>
2024-12-27 16:11:31 +0100

    added django

--- iot/iot/settings.py
+++ iot/iot/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-#qkx1g6okg3dgdfw5notn56phdl-a3q3v*w%70a_8v@3zwao1e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','*']
+ALLOWED_HOSTS = ['localhost', 'anass-afbzamh7gta5djfn.canadacentral-01.azurewebsites.net','*']
 
 
 
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
